afa25ea09c8a6de9048ad19902c265528f2add9a
hummingbird-12 <entropyih@sogang.ac.kr>
2020-08-27 08:26:20 +0900

    GPIO - Serial test

---
+++ GPIO/2_Serial_test/GPIO_test_serial.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+#include <cstring>
+#include <wiringPi.h>
+#include <wiringSerial.h>
+
+#define GPIO0 0 // Physical 11
+#define GPIO3 3 // Physical 15
+
+#define SERIAL_DEV "/dev/ttyS0"
+#define BAUD_RATE 115200
+
+int main(void) {
+    int fd;
+    unsigned char test, receive_char;
+
+    if (wiringPiSetup() == -1) {
+        printf("WiringPi setup error!\n");
+        return -1;
+    }
+
+    if ((fd = serialOpen(SERIAL_DEV, BAUD_RATE)) < 0) {
+        printf("UART open error!\n");
+        return -1;
+    }
+
+    test = 'A';
+    while (true) {
+        serialPutchar(fd, test);
+        delay(1);
+
+        if (serialDataAvail(fd)) {
+            receive_char = serialGetchar(fd);
+            printf("Received char : %d %c\n", receive_char, receive_char);
+        }
+    }
+
+    return 0;
+}
